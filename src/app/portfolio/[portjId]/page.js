8e4a7e4ccce05d129928d01c8df5f09468b6a518
AlleMacchi <alessandro.macchitella@gmail.com>
2023-11-28 20:59:01 +1100

    Test per capire problema server v02

--- src/app/portfolio/[portjId]/page.js
+++ src/app/portfolio/[portjId]/page.js
@@ -5,19 +5,28 @@ import Data from '../../../data/Portfolio/Proj.json'
 
 import ParseTxtToHtml from '../components/ParseTxtToHtml'
 
-import { readFileSync } from 'fs';
+// import { readFileSync } from 'fs';
 
 export default function ProjectPage({params}){
        
-const Overview = readFileSync('./public/' + Data[params.portjId].overview.file, 'utf-8');
-const Paragraph01 = readFileSync('./public/' + Data[params.portjId].paragraph01.file, 'utf-8');
-const Paragraph02 = readFileSync('./public/' + Data[params.portjId].paragraph02.file, 'utf-8');
-const Paragraph03 = readFileSync('./public/' + Data[params.portjId].paragraph03.file, 'utf-8');
-const UseTech = readFileSync('./public/' + Data[params.portjId].usedTech.file, 'utf-8');
-const urlMedia01 = readFileSync('./public/' + Data[params.portjId].urlMedia01.file, 'utf-8');
-const urlMedia02 = readFileSync('./public/' + Data[params.portjId].urlMedia02.file, 'utf-8');
-const urlMedia03 = readFileSync('./public/' + Data[params.portjId].urlMedia03.file, 'utf-8');
-const Conclusion = readFileSync('./public/' + Data[params.portjId].conclusion.file, 'utf-8');
+// const Overview = readFileSync('./public/' + Data[params.portjId].overview.file, 'utf-8');
+// const Paragraph01 = readFileSync('./public/' + Data[params.portjId].paragraph01.file, 'utf-8');
+// const Paragraph02 = readFileSync('./public/' + Data[params.portjId].paragraph02.file, 'utf-8');
+// const Paragraph03 = readFileSync('./public/' + Data[params.portjId].paragraph03.file, 'utf-8');
+// const UseTech = readFileSync('./public/' + Data[params.portjId].usedTech.file, 'utf-8');
+// const urlMedia01 = readFileSync('./public/' + Data[params.portjId].urlMedia01.file, 'utf-8');
+// const urlMedia02 = readFileSync('./public/' + Data[params.portjId].urlMedia02.file, 'utf-8');
+// const urlMedia03 = readFileSync('./public/' + Data[params.portjId].urlMedia03.file, 'utf-8');
+// const Conclusion = readFileSync('./public/' + Data[params.portjId].conclusion.file, 'utf-8');
+const Overview = 'Overview';
+const Paragraph01 = '<p>Paragraph01</p>';
+const Paragraph02 = '<p>Paragraph02</p>';
+const Paragraph03 = '<p>Paragraph03</p>';
+const UseTech = '<p>Paragraph01</p>';
+const urlMedia01 = '<p>urlMedia01</p>';
+const urlMedia02 = '<p>urlMedia02</p>';
+const urlMedia03 = '<p>urlMedia03</p>';
+const Conclusion = '<p>Conclusion</p>';
 
 
 return(
@@ -42,7 +51,7 @@ return(
 
                     <h2>used technology</h2>
                     <div className={style.techBx}>
-                        {/* <ParseTxtToHtml Txt={UseTech} />   */}
+                        <ParseTxtToHtml Txt={UseTech} />  
                     </div>
                     <h2>Country</h2>
                     <h4>{Data[params.portjId].country}</h4>
@@ -56,28 +65,28 @@ return(
             </div>
             <div className={style.contentBx}>
                 <div className={style.paragraph01}>
-                    {/* <ParseTxtToHtml Txt={Paragraph01} />   */}
+                    <ParseTxtToHtml Txt={Paragraph01} />  
                 </div>
                 <div className={style.paragraph02}>
-                    {/* <ParseTxtToHtml Txt={Paragraph02} />  */}
+                    <ParseTxtToHtml Txt={Paragraph02} /> 
                 </div>
                 <div className={style.image01}>
-                    {/* <ParseTxtToHtml Txt={urlMedia01} />   */}
+                    <ParseTxtToHtml Txt={urlMedia01} />  
                 </div>
                 <div className={style.paragraph03}>
-                    {/* <ParseTxtToHtml Txt={Paragraph03} /> */}
+                    <ParseTxtToHtml Txt={Paragraph03} />
                 </div>
                 <div className={style.image02}>
-                    {/* <ParseTxtToHtml Txt={urlMedia02} /> */}
+                    <ParseTxtToHtml Txt={urlMedia02} />
                 </div>
             </div>
             
             <div className={style.conclusionBx}>
                 <div className={style.conculsionImage} id={style.conclusionIdImage}>                    
-                    {/* <ParseTxtToHtml Txt={urlMedia03} /> */}
+                    <ParseTxtToHtml Txt={urlMedia03} />
                 </div>
                 <div className={style.conclusionText} id={style.conclusionIdText}>
-                    {/* <ParseTxtToHtml Txt={Conclusion} /> */}
+                    <ParseTxtToHtml Txt={Conclusion} />
                 </div>
             </div>
             <div className={style.titleBx}>
