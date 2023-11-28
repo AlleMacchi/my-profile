b80ed782e9280806ac8b14189d30061da34bf125
AlleMacchi <alessandro.macchitella@gmail.com>
2023-11-28 13:48:17 +1100

    Test Portfolio Page with use client

--- src/app/portfolio/[portjId]/page.js
+++ src/app/portfolio/[portjId]/page.js
@@ -1,3 +1,4 @@
+"use client"
 import style from '../../../styles/pages/portfolio/components/portfolioPage/style.module.css'
 import Title from '../../../components/title/title'
 import GeneralData from '../../../data/Portfolio/List.json'
