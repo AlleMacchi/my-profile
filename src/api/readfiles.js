820784cd19d773f136ec2e2ca644790dcb22baaf
AlleMacchi <alessandro.macchitella@gmail.com>
2023-11-29 09:49:13 +1100

    Change API

--- src/api/readfiles.js
+++ src/api/readfiles.js
@@ -1,9 +1,9 @@
-import { promises as fs } from 'fs';
+import fs from 'fs';
 import path from 'path';
 
-export default async function ReadFiles(props) {
+export default function ReadFiles(props) {
   const filePath = props.path;
-  const buffer = await fs.readFileSync(path.join(process.cwd(), 'public', filePath), 'utf-8');
+  const buffer = fs.readFileSync(path.join(process.cwd(), 'public', filePath), 'utf-8');
   const fileContent = buffer.toString();
 
   return (
