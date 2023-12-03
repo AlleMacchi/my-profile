89c9cbd32a9a627119450c138b72e4a03b677083
AlleMacchi <alessandro.macchitella@gmail.com>
2023-12-03 12:08:32 +1100

    Update Setting

--- src/app/project05/setting/page.js
+++ src/app/project05/setting/page.js
@@ -1,10 +1,55 @@
-import React from "react";
+"use client"
+import React, { useState }  from "react";
 import style from '../../../styles/pages/project05/setting/style.module.css'
 
-export default function Setting(){
-    return(       
-        <div className={style.container} >
-            <h2>Setting</h2>         
-        </div>
-        )
-}
+const dataArray = [
+  { id: 1, name: "Item 1", price: 10.00 },
+  { id: 2, name: "Item 2", price: 19.00 },
+  { id: 3, name: "Item 3", price: 7.00 },
+  { id: 4, name: "Item 4", price: 8.00 },
+];
+
+
+
+
+const Setting = () => {
+  const [updatedArray, setUpdatedArray] = useState([...dataArray]);
+  const [editedPrices, setEditedPrices] = useState({});
+
+  const handleEditChange = (itemId, value) => {
+    setEditedPrices((prev) => ({ ...prev, [itemId]: value }));
+  };
+
+  const updatePrice =  async (itemId) => {
+    const newPrice = parseFloat(editedPrices[itemId]);
+    if (!isNaN(newPrice)) {
+      const updatedItems = updatedArray.map((item) =>
+        item.id === itemId ? { ...item, price: newPrice } : item
+      );
+      setUpdatedArray(updatedItems);
+    }
+  };
+
+
+  return (
+    <div className={style.container}>
+      <h1>Setting</h1>
+      <ul>
+        {updatedArray.map((item) => (
+          <li key={item.id}>
+            <strong>{item.name}</strong> - $
+            <strong>{item.price}</strong>
+            <input
+              type="number"
+              value={editedPrices[item.id] || item.price}
+              onChange={(e) => handleEditChange(item.id, e.target.value)}
+            />
+            <button onClick={() => updatePrice(item.id)}>Confirm</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Setting;
